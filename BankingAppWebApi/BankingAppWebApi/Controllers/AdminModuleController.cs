775fb91fd7fb3b4e8c1aabb567293c2a70d7e869
inveka <inveka@DTGBPET00276>
2021-03-30 13:06:18 +0100

    changes done in branch 1

--- BankingAppWebApi/BankingAppWebApi/Controllers/AdminModuleController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/AdminModuleController.cs
@@ -10,6 +10,7 @@ using System.Web.Http.Cors;
 namespace BankingAppWebApi.Controllers
 {
     //done change 1
+    //changes done in branch 1
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class AdminModuleController : ApiController
     {
