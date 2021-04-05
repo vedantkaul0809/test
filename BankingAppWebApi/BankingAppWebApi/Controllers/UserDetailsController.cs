285d788b2b068dd681121e2d8296da4a412bd4eb
vedantkaul0809 <vedant_kaul@yahoo.co.in>
2021-04-05 18:18:38 +0530

    commit in branch3 UserDetailsController

--- BankingAppWebApi/BankingAppWebApi/Controllers/UserDetailsController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/UserDetailsController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 
 namespace BankingAppWebApi.Controllers
 {
-    
+    //change in userdetails controller
     
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UserDetailsController : ApiController
