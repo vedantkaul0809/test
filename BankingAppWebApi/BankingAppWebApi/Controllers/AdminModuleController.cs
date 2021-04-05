44dc0d1cb1e2089713b6839993b45a643fe5a519
vedantkaul0809 <vedant_kaul@yahoo.co.in>
2021-04-05 12:55:14 +0530

    change

--- BankingAppWebApi/BankingAppWebApi/Controllers/AdminModuleController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/AdminModuleController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 namespace BankingAppWebApi.Controllers
 {
    
-  //change in admin
+  
     
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class AdminModuleController : ApiController
