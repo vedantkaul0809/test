ce9e173986d38499b09f86e22988bfbe9f80f061
vedantkaul0809 <vedant_kaul@yahoo.co.in>
2021-04-06 12:40:54 +0530

    commit 1 in branch 1

--- BankingAppWebApi/BankingAppWebApi/Controllers/CustomerController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/CustomerController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 
 namespace BankingAppWebApi.Controllers
 {
-   
+   //change 1 done in customer controller by branch1
     
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CustomerController : ApiController
