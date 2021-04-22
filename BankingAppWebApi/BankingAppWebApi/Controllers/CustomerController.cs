dfc965189d25efaad47506718a3fc1fc3d2da6e1
vedantkaul0809 <vedant_kaul@yahoo.co.in>
2021-04-22 12:41:44 +0530

    commit1

--- BankingAppWebApi/BankingAppWebApi/Controllers/CustomerController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/CustomerController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 
 namespace BankingAppWebApi.Controllers
 {
-    
+    //change 1 in customer using cherrpickbranch
     
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CustomerController : ApiController
