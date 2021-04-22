939af2fd527f187fe2d3d9dc42a939d76fc68960
vedantkaul0809 <vedant_kaul@yahoo.co.in>
2021-04-22 12:30:11 +0530

    commit 1 on transaction by localbranch

--- BankingAppWebApi/BankingAppWebApi/Controllers/TransactionController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/TransactionController.cs
@@ -11,7 +11,7 @@ using System.Web.Http.Cors;
 
 namespace BankingAppWebApi.Controllers
 {
-    
+    //change 1 done in transaction
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class TransactionController : ApiController
     {
