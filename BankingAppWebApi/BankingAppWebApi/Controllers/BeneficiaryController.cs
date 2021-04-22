5c493297e0a1d586474c1bbcb1226a296e9f29af
vedantkaul0809 <vedant_kaul@yahoo.co.in>
2021-04-22 12:21:52 +0530

    commit 1 on branch1

--- BankingAppWebApi/BankingAppWebApi/Controllers/BeneficiaryController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/BeneficiaryController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 
 namespace BankingAppWebApi.Controllers
 {
-    
+    //change 1 in beneficiary
   
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class BeneficiaryController : ApiController
