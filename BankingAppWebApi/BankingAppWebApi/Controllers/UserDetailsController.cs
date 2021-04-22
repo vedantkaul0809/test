b026ddb80bb30d15ec893620022eecdd5e275aae
testgithub159 <vedant1998kaul@gmail.com>
2021-04-22 08:36:27 +0100

    commit

--- BankingAppWebApi/BankingAppWebApi/Controllers/UserDetailsController.cs
+++ BankingAppWebApi/BankingAppWebApi/Controllers/UserDetailsController.cs
@@ -11,7 +11,7 @@ using System.Web.Http.Cors;
 namespace BankingAppWebApi.Controllers
 {
     //change 1 in userdetails using pull request branch
-    
+    //change 2 done by testgithub
    
 
     [EnableCors(origins: "*", headers: "*", methods: "*")]
